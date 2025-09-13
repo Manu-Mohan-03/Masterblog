a40348dd8db6c83e026096d7386b1502dc0e0ea4
Manu Mohan <manu.mohan0303@gmail.com>
2025-09-14 00:00:43 +0200

    Inline comments

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """Home page"""
     # to fetch the blog posts from a file
     blog_posts = blogs.load_data()
     return render_template('index.html', posts=blog_posts)
@@ -13,23 +14,31 @@ def index():
 
 @app.route('/add', methods=['GET', 'POST'])
 def add():
+    """To create a new blog"""
     if request.method == 'POST':
         # Add the code that handles adding a new blog
-        blog_post = request.form.get()
+        blog_post = {}
+        blog_post['title'] = request.form.get('title')
+        blog_post['author'] = request.form.get('author')
+        blog_post['content'] = request.form.get('content')
         blogs.post_data(blog_post)
+        # Redirect back to index
         return redirect(url_for('index'))
+    # GET request means render the page for adding new blog
     return render_template('add.html')
 
 
 @app.route('/delete/<int:post_id>')
 def delete(post_id):
+    """To delete an existing blog"""
     blogs.delete_data(post_id)
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    # Redirect back to index
+    return redirect(url_for('index'))
 
 
-@app.route('/update/<post_id>', methods = ['GET', 'POST'])
+@app.route('/update/<int:post_id>', methods = ['GET', 'POST'])
 def update(post_id):
+    """To update an exising blog"""
     # Fetch the blog posts from the JSON file
     post = blogs.fetch_post_by_id(post_id)
     if post is None:
@@ -37,10 +46,17 @@ def update(post_id):
         return "Post not found", 404
     if request.method == 'POST':
         # Update the post in the JSON file
+        blog_post = {}
+        blog_post['id'] = post_id
+        blog_post['title'] = request.form.get('title')
+        blog_post['author'] = request.form.get('author')
+        blog_post['content'] = request.form.get('content')
+        blogs.update_data(blog_post)
         # Redirect back to index
-        blog = request.form.get()
-        blogs.update_data(post_id,blog)
-    else:
-        # Else, it's a GET request
-        # So display the update.html page
-        return render_template('update.html', post=post)
+        return redirect(url_for('index'))
+    # Else, it's a GET request, so go to update blog page
+    return render_template('update.html', post=post)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5001, debug=True)
--- data_storage.py
+++ data_storage.py
@@ -4,38 +4,47 @@ JSON_FILE = 'data/blogs.json'
 
 
 def load_data():
+    """To read all the blogs from JSON file"""
     with open(JSON_FILE,"r",encoding='UTF-8') as handle:
         return json.load(handle)
 
+
 def save_data(blog_posts):
+    """To save(overwrite) all the details to JSON file"""
     with open(JSON_FILE, "w") as handle:
         return json.dump(blog_posts, handle, indent=4)
 
-def post_data(post, filepath):
-   blog_posts = load_data()
-   if blog_posts:
-       max_id = max([blog['id'] for blog in blog_posts])
-       post_id = max_id + 10
-       post['id'] = post_id
-   else:
-       post['id'] = 10
-   post_id = str(len(blog_posts)*10 + 10)
-
-   #post[post_id] = post
-   blog_posts.append(post)
-   save_data(blog_posts)
-
-
-def delete_data(post_id):
+def post_data(post):
+    """To add a new blog to JSON"""
     blog_posts = load_data()
-    for blog in blog_posts:
-        if blog['id'] == post_id:
-            blog_posts.remove(blog)
-            break
+    if blog_posts:
+        max_id = max([blog['id'] for blog in blog_posts])
+        post_id = max_id + 10
+        post['id'] = post_id
+    else:
+        post['id'] = 10
+    # Add blogs to JSON
+    blog_posts.append(post)
+    # Save JSON
     save_data(blog_posts)
 
 
+def delete_data(post_id):
+    """To delete blog from JSON file"""
+    blog_posts = load_data()
+    # To find the matching blog
+    for blog in blog_posts:
+        if blog['id'] == post_id:
+            #Removed from JSON
+            blog_posts.remove(blog)
+            # Save JSON
+            save_data(blog_posts)
+            return True
+    return None
+
+
 def fetch_post_by_id(post_id):
+    """To fetch a specific blog details using the ID(post_id)"""
     blog_posts = load_data()
     for blog in blog_posts:
         if blog['id'] == post_id:
@@ -43,15 +52,22 @@ def fetch_post_by_id(post_id):
     return None
 
 
-def update_data(post_id, new_blog):
+def update_data(new_blog):
+    """Update the blog. id will be same and is a key in new_blog"""
     blog_posts = load_data()
+    # Fetching the old blog with same ID
     for blog in blog_posts:
-        if blog['id'] == post_id:
+        if blog['id'] == new_blog['id']:
             old_blog = blog
-            break
-    index = blog_posts.index(old_blog)
-    blog_posts[index] = new_blog
-    save_data(blog_posts)
+            # To find the index of old blog
+            index = blog_posts.index(old_blog)
+            # Adding new_blog @ same index
+            blog_posts[index] = new_blog
+            # Saving JSON
+            save_data(blog_posts)
+            return True
+    return None
+
 
 
 
